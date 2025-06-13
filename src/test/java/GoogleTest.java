93e5d2cc27fff3973a04a448cc0b91adc39f0bef
mihannizzm <miha.bush@gmail.com>
2025-06-13 15:55:49 +0400

    initial commit

--- src/test/java/GoogleTest.java
+++ src/test/java/GoogleTest.java
@@ -1,9 +1,17 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.After;
 import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class GoogleTest {
+
+    @AfterEach
+    void afterEach() {
+        Configuration.remote = "http://88.210.20.169/4444";
+    }
 
     @Test
     public void testExample() {
