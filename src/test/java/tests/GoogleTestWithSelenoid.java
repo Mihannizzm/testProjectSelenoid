fb41f6f9c7a4f7af0cd4064f5f8a50217482a5fc
mihannizzm <miha.bush@gmail.com>
2025-06-14 11:19:03 +0400

    initial commit

--- src/test/java/tests/GoogleTestWithSelenoid.java
+++ src/test/java/tests/GoogleTestWithSelenoid.java
@@ -59,9 +59,9 @@ public class GoogleTestWithSelenoid {
         String host = "http://" + selenoidHost + ":" + selenoidPort + "/wd/hub";
 
         selenoidUi = new GenericContainer<>(DockerImageName.parse("aerokube/selenoid-ui"))
-                .withExposedPorts(8080, 32853)
+//                .withExposedPorts(8080)
                 .withNetwork(network)
-                .withCommand("--selenoid-uri=http://selenoid:%s".formatted(
+                .withCommand("-p 8080:32853 --selenoid-uri=http://selenoid:%s".formatted(
                         4444
                 ));
         selenoidUi.start();
